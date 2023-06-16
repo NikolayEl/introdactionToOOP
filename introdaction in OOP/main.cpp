2df202f3d2d99491f7e98ce7a958256b7b44810f
Nikolay Elshin <nikolay19830223@gmail.com>
2023-06-16 22:17:52 +0800

    class is over

--- introdaction in OOP/main.cpp
+++ introdaction in OOP/main.cpp
@@ -6,11 +6,33 @@ class Point
 {
 	double x;
 	double y;
+public:
+	double get_x(void) const
+	{
+		return x;
+	}
+	double get_y() const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
 };
+
+//#define STRACT_POINT
 
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef STRACT_POINT
+
+
 	//type name;
 	int a; // Объявление переменной 'а' типо 'int'
 	Point A; //Объявление переменной 'А' типа 'Poin'
@@ -23,5 +45,12 @@ void main()
 	Point* pA = &A;
 	cout << pA->x << "\t" << pA->y << endl;
 
-	
+#endif // STRACT_POINT
+
+	Point A;
+
+	A.set_x(2);
+	A.set_y(3);
+
+	cout << A.get_x() << "\t" << A.get_y() << endl;
 }
