c11e1c90a8d50841f8e126c07826f723dce8b44e
Nikolay Elshin <nikolay19830223@gmail.com>
2023-07-07 18:15:53 +0800

    get, set methods, operator=

---
+++ String/main.cpp
@@ -0,0 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+
+class String
+{
+	int size; //Размер строки в байтах
+	char* str; //Адрес строки в динамической памяти
+public:
+	//				Get method
+	char* get_str(void) const
+	{
+		return str;
+	}
+	//				Set method
+	void set_str(const char* str)
+	{
+		this->str = (char*)str;
+	}
+	//				Constructors
+	String(int size = 80)
+	{
+		this->size = size;
+		this->str = new char[size] {};
+		cout << "DefConstructor:\t" << this << endl;
+	}
+
+	String (const char* str)
+	{
+		this->str = (char*)str;
+		cout << "1ArgDConstructor:\t" << this << endl;
+	}
+
+	~String()
+	{
+		//if(str)delete[] str;
+		cout << "Destructor:\t" << this << endl;
+	}
+
+	//				Methods
+	void print() const
+	{
+		cout << "Size:\t" << size << endl;
+		cout << "Str:\t" << str << endl;
+
+	}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	cout << sizeof("Hello") << endl;
+	String str;
+	str.set_str("Test");
+	cout << str.get_str() << endl;
+	str.print();
+	String str2 = 5;
+	str2.print();
+	String str3 = "Hello";
+	str.print();
+}
